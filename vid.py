a75b4df8064cffc31eb066e7613e52bef5904994
Devan-Soliman <95260363+Devan-Soliman@users.noreply.github.com>
2022-12-25 21:33:58 -0800

    Create vid.py

---
+++ vid.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as GPIO
+import subprocess
+from datetime import datetime
+
+button = 5
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+while(True):
+    GPIO.wait_for_edge(button, GPIO.FALLING)
+    # Record 30 minute segments encoded in H.264
+    subprocess.Popen(f"libcamera-vid -o {datetime.now().isoformat()}.mkv --codec h264 --width 1920 --height 1080 -s --segment 1800000 -t 0 &", shell=True)
+    
+    GPIO.wait_for_edge(button, GPIO.FALLING)
+    # Exit libcamera-vid
+    subprocess.Popen("pkill -SIGUSR2 libcamera-vid", shell=True)
