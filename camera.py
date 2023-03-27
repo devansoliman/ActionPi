7e2f096431fd6cc4c45e57823e008f1c0390d3b3
Devan-Soliman <95260363+Devan-Soliman@users.noreply.github.com>
2023-03-26 20:39:01 -0700

    Update camera.py
    added support for taking photos while video is recording

--- camera.py
+++ camera.py
@@ -3,6 +3,9 @@ from picamera2.encoders import H264Encoder
 from picamera2 import Picamera2
 from datetime import datetime
 
+# video recording state
+recording = False
+
 # button colors
 red = 5
 white = 6
@@ -22,15 +25,31 @@ encoder = H264Encoder(bitrate=10000000)
 picam2.start(show_preview=True)
 
 def video():
-    timestamp = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-    picam2.start_recording(encoder, f"{timestamp}.h264")
-    GPIO.wait_for_edge(red, GPIO.FALLING)
-    picam2.stop_recording()
+    global recording
+    if recording == False:
+        timestamp = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    
+        try:
+            picam2.start_recording(encoder, f"{timestamp}.h264")
+            recording = True
+        except:
+            recording = False
+    else:
+        picam2.stop_recording()
 
-# switch from preview to capture mode and save to file
+
 def photo():
+    global recording
     timestamp = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-    picam2.switch_mode_and_capture_file(capture_config, f"{timestamp}.jpg")
+    
+    if recording == False:
+        # switch from preview to capture mode and save to file
+        picam2.switch_mode_and_capture_file(capture_config, f"{timestamp}.jpg")
+    else:
+        # capture image while video is recording
+        request = picam2.capture_request()
+        request.save("main", f"{timestamp}.jpg")
+        request.release()
 
 # event detection for colored buttons
 GPIO.add_event_detect(red, GPIO.RISING, video, 150)
