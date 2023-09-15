4ca36ca257eceb5a57e053c10115d153f26eb9e1
Devan-Soliman <95260363+DevanSoliman@users.noreply.github.com>
2023-09-15 09:53:30 -0700

    status LED + main power switch

--- camera.py
+++ camera.py
@@ -10,11 +10,15 @@ recording = False
 red = 5
 white = 6
 
+# status LED
+led = 13
+
 # configure GPIO
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(red, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(white, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(led, GPIO.OUT)
 
 # configure picamera2
 picam2 = Picamera2()
@@ -32,20 +36,25 @@ def video():
         try:
             picam2.start_recording(encoder, f"{timestamp}.h264")
             recording = True
+            GPIO.output(led, GPIO.HIGH)
         except:
             recording = False
+            GPIO.output(led, GPIO.LOW)
 
     else:
         picam2.stop_recording()
         recording = False
+        GPIO.output(led, GPIO.LOW)
 
 def photo():
     global recording
     timestamp = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
     
     if recording == False:
+        GPIO.output(led, GPIO.HIGH)
         # switch from video to still config, save to file, and switch back
         picam2.switch_mode_and_capture_file(still_config, f"{timestamp}.jpg")
+        GPIO.output(led, GPIO.LOW)
     else:
         # capture image while video is recording
         request = picam2.capture_request()
