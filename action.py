fdb4d1f70c7b47f2624cb52fc01232e7f06a9940
Devan Soliman <95260363+DevanSoliman@users.noreply.github.com>
2024-07-28 18:19:40 -0700

    core functionality update
    - remove status led
    - remove photo button
    - add oled (ssd1306)

--- action.py
+++ action.py
@@ -6,69 +6,70 @@ from datetime import datetime
 import time
 from signal import pause
 
+from luma.core.interface.serial import i2c, spi, pcf8574
+from luma.core.interface.parallel import bitbang_6800
+from luma.core.render import canvas
+from luma.oled.device import ssd1306
+
+from PIL import Image
+
+
 # video recording state
 recording = False
 
-# button colors
-red = 5
-white = 6
-
-# status LED
-led = 13
+# pin assignment
+button = 5
 
 # configure GPIO
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(red, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(white, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(led, GPIO.OUT)
+GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# initialize display
+serial = i2c(port=1, address=0x3C)
+device = ssd1306(serial)
+
+# splash image
+splash =  Image.open("splash.jpg")
+size = device.width, device.height
+splash = splash.resize(size, Image.LANCZOS)
+device.display(splash.convert(device.mode))
 
 # configure picamera2
 picam2 = Picamera2()
 video_config = picam2.create_video_configuration(transform=Transform(hflip=1, vflip=1))
-still_config = picam2.create_still_configuration(transform=Transform(hflip=1, vflip=1))
 picam2.configure(video_config)
 encoder = H264Encoder(bitrate=10000000)
 picam2.start()
 
-# startup blink
-for i in range(3):
-    GPIO.output(led, GPIO.HIGH)
-    time.sleep(0.1)
-    GPIO.output(led, GPIO.LOW)
-    if i < 2:
-        time.sleep(0.1)
+# ready image
+ready =  Image.open("ready.jpg")
+ready = ready.resize(size, Image.LANCZOS)
+device.display(ready.convert(device.mode))
 
-def video(red):
+# active image
+active =  Image.open("active.jpg")
+active = active.resize(size, Image.LANCZOS)
+
+print("READY")
+
+def video(button):
     global recording
 
     if recording == False:
         timestamp = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
         picam2.start_encoder(encoder, f"{timestamp}.h264")
         recording = True
-        GPIO.output(led, GPIO.HIGH)
+        print("RECORDING")
+        device.display(active.convert(device.mode))
+
     else:
         picam2.stop_encoder()
         recording = False
-        GPIO.output(led, GPIO.LOW)
+        print("STOPPED")
+        device.display(ready.convert(device.mode))
 
-def photo(white):
-    global recording
-    timestamp = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-
-    if recording == False:
-        GPIO.output(led, GPIO.HIGH)
-        # switch from video to still config, save to file, and switch back
-        picam2.switch_mode_and_capture_file(still_config, f"{timestamp}.jpg")
-        GPIO.output(led, GPIO.LOW)
-    else:
-        # capture image while video is recording
-        request = picam2.capture_request()
-        request.save("main", f"{timestamp}.jpg")
-        request.release()
-
-# event detection for colored buttons
-GPIO.add_event_detect(red, GPIO.FALLING, video, bouncetime=100)
-GPIO.add_event_detect(white, GPIO.FALLING, photo, bouncetime=100)
+# button event detection
+GPIO.add_event_detect(button, GPIO.FALLING, video, bouncetime=250)
 
 pause()
